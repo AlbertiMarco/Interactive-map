6967555f2efbfcd02e04eb4f4f419756311a4504
Marco Alberti <42811127+AlbertiMarco@users.noreply.github.com>
2021-04-16 02:17:47 +0200

    Update Interactive_Healthcare_Map.py

--- Interactive_Healthcare_Map.py
+++ Interactive_Healthcare_Map.py
@@ -12,7 +12,7 @@ from bokeh.models import Select, HoverTool
 from bokeh.layouts import widgetbox, row, column
 
 #Import Data
-mun=gpd.read_file('data/data.json')
+mun=gpd.read_file('data/data.shp')
 data=mun[mun['year']==2016] #select a single year
 nl_data_json = json.loads(data.to_json())
 json_data = json.dumps(nl_data_json)
